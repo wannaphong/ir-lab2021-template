3c8f3532ef993e52d181ffd99c089ca645b03d85
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-03-02 14:02:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def read_root():
 @app.get("/text/{text}")
 def read_word(text: str):
     return {"word": text}
+
+if __name__ == '__main__':
+   import uvicorn
+   uvicorn.run(app, host="0.0.0.0", port=5000, debug=True) 
